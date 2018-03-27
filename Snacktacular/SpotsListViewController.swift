a7d82e5f518816cc984bdbb698287eec4dbc4bfb
John Gallaugher <john.gallaugher@bc.edu>
2018-03-27 14:09:21 -0400

    Start here when working through the "Snacktacular" chapter

---
+++ Snacktacular/SpotsListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snacktacular
+//
+//  Created by John Gallaugher on 3/23/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class SpotsListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
