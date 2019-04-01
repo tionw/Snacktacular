c702989b354acce9d72c0bb17063b66f7ee954c9
user150978 <user150978@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-04-01 17:22:00 -0400

    Add Map and Location support

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import GooglePlaces
+import MapKit
+import Contacts
 
 class SpotDetailViewController: UIViewController {
     
@@ -16,22 +18,45 @@ class SpotDetailViewController: UIViewController {
     @IBOutlet weak var averageRatingLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var tableView: UITableView!
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     var spot: Spot!
+    let regionDistance: CLLocationDistance = 750
+    var locationManager: CLLocationManager!
+    var currentLocation: CLLocation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //mapView.delegate = self
+        
         if spot == nil {
             spot = Spot()
+            getLocation()
         }
-        nameField.text = spot.name
-        addressField.text = spot.address
+        
+        let region = MKCoordinateRegion(center: spot.coordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
+        mapView.setRegion(region, animated: true)
+        updateUserInterface()
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
     }
     
     func updateUserInterface() {
         nameField.text = spot.name
         addressField.text = spot.address
+        updateMap()
+    }
+    
+    func updateMap() {
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotation(spot)
+        mapView.setCenter(spot.coordinate, animated: true)
     }
     
     func leaveViewController(){
@@ -50,6 +75,8 @@ class SpotDetailViewController: UIViewController {
     }
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        spot.name = nameField.text!
+        spot.address = addressField.text!
         spot.saveData { success in
             if success {
                 self.leaveViewController()
@@ -99,4 +126,62 @@ extension SpotDetailViewController: GMSAutocompleteViewControllerDelegate {
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
     
+}
+
+extension SpotDetailViewController: CLLocationManagerDelegate{
+    
+    func getLocation(){
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func handleLocationAuthorizationStatus(status: CLAuthorizationStatus){
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.requestLocation()
+        case.denied:
+            print("I'm sorry - can't show location.")
+        case.restricted:
+            print("Access denied.")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        handleLocationAuthorizationStatus(status: status)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard spot.name == "" else {
+            return
+        }
+        let geoCoder = CLGeocoder()
+        var name = ""
+        var address = ""
+        currentLocation = locations.last
+        spot.coordinate = currentLocation.coordinate
+        geoCoder.reverseGeocodeLocation(currentLocation, completionHandler:
+            {placemarks, error in
+                if placemarks != nil{
+                    let placemark = placemarks?.last
+                    name = placemark?.name ?? "name unknown"
+                    if let postalAddress = placemark?.postalAddress {
+                        address = CNPostalAddressFormatter.string(from: postalAddress, style: .mailingAddress)
+                    }
+                    
+                } else{
+                    print("Error retrieving place. Error code:\(error!)")
+                }
+                self.spot.name = name
+                self.spot.address = address
+                self.updateUserInterface()
+                
+        }
+        )
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get user location.")
+    }
 }
